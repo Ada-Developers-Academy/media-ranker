a8da26a2e1a3159b5dee1a6b8cc2137589da07a1
Bookis Smuin <bookis.smuin@gmail.com>
2014-01-01 20:14:50 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MediaRanker::Application.config.secret_key_base = '0df28f9ce04374e8c26b46139763ed45808842d06c94e575925a7c2671f76b6434ae33c2e0d776f9ff21ff6f251c0d2161fbe714f6aaccee3b17be0fff606973'
